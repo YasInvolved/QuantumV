88994560e6449fefd3024920cfb34a46d78e73b4
YasInvoled <szrtxm.op@gmail.com>
2024-09-27 00:24:10 +0200

    correct entry point a bit

--- QuantumV/include/QuantumV/entry_point.h
+++ QuantumV/include/QuantumV/entry_point.h
@@ -2,10 +2,40 @@
 
 extern QuantumV::Application* QuantumV::CreateApplication();
 
-int main(int argc, char** argv) {
-	QuantumV::Log::Init();
-	auto app = QuantumV::CreateApplication();
-	app->Run();
-	delete app;
-	return 0;
-}
+#ifdef QV_PLATFORM_WINDOWS
+	#ifdef QV_DEBUG
+
+	int main(int argc, char** argv) {
+		QuantumV::Log::Init();
+		auto app = QuantumV::CreateApplication();
+		app->Run();
+		delete app;
+		return 0;
+	}
+
+	#else
+
+	#define WIN32_LEAN_AND_MEAN
+	#include <Windows.h>
+
+	int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, PSTR cmdLine, int nCmdShow) {
+		QuantumV::Log::Init();
+		auto app = QuantumV::CreateApplication();
+		app->Run();
+		delete app;
+		return 0;
+	}
+
+	#endif
+#else
+
+	#error platforms other than windows are not supported for now
+	int main(int argc, char** argv) {
+		QuantumV::Log::Init();
+		auto app = QuantumV::CreateApplication();
+		app->Run();
+		delete app;
+		return 0;
+	}
+
+#endif
